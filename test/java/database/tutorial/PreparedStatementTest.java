7fb70380c9a3f7c46938c45d94020614f2b6e34a
jekson tambunan <108537531+jekson11@users.noreply.github.com>
2023-08-12 21:29:34 +0700

    Update PreparedStatementTest.java
    delete comment

--- test/java/database/tutorial/PreparedStatementTest.java
+++ test/java/database/tutorial/PreparedStatementTest.java
@@ -5,20 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.sql.*;
 
 public class PreparedStatementTest {
-/*
-    PreparedStatement adalah turunan dari Statement, jadi apapun yang bisa dilakukan Statement, bisa dilakukan juga oleh PreparedStatement
-    Yang membedakan PreparedStatement dari Statement adalah, PreparedStatement memiliki kemampuan untuk mengamankan input dari user sehingga aman dari serangan SQL Injection
 
-    Berbeda dengan Statement, pada PreparedStatement, ketika pembuatannya, sudah ditentukan SQL apa yang akan kita gunakan
-    Oleh karena itu, PreparedStatement biasanya digunakan untuk sekali mengirim perintah SQL, jika ingin mengirim perintah SQL lagi, kita harus membuat PreparedStatement baru
-    Untuk membuat PreparedStatement, kita bisa menggunakan method prepareStatement(sql) milik Connection
-
-    Sekarang pertanyaannya, bagaimana cara menerima input user menggunakan PreparedStatement?
-    Untuk menerima input dari user, SQL yang kita buat harus diubah juga
-    Input dari user, perlu kita ubah menjadi ? (tanda tanya)
-    Nanti ketika pembuatan object, kita bisa subtitusi datanya menggunakan setXxx(index, value) sesuai dengan tipe datanya, misal setString(), setInt() dan lain-lain
-
- */
     @Test
     void testPreparedStatement() throws SQLException {
         Connection connection = ConnectionUtil.getDataSource().getConnection();
