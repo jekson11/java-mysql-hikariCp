1728e5d5008cc6ab1a3db2b834ece08d498b3ac4
jekson tambunan <108537531+jekson11@users.noreply.github.com>
2023-08-12 21:28:42 +0700

    Update GetAutoIncrementMysqlTest.java
    delete comment

--- test/java/database/tutorial/GetAutoIncrementMysqlTest.java
+++ test/java/database/tutorial/GetAutoIncrementMysqlTest.java
@@ -5,19 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.sql.*;
 
 public class GetAutoIncrementMysqlTest {
-    /*
-    Kadang setelah melakukan INSERT data ke database yang memiliki primary key auto increment, kita ingin mendapatkan data id terbarunya
-    di JDBC, ada kemampuan untuk mendapatkan auto generate data seperti auto increment dengan method getGenerateKeys() yang mengembalikan ResultSet
-    Selanjutnya kita bisa melakukan iterasi terhadap ResultSet tersebut
-
-    Secara default, Statement ataupun PreparedStatement tidak mengerti untuk mengambil auto generate key
-    Kita perlu memberi tahunya agar Statement ataupun PreparedStatement mengambil auto generate id secara otomatis
-    Untuk Statement, kita perlu memberi tahu ketika memanggil method executeUpdate(sql, Statement.RETURN_GENERATED_KEYS)
-    Sedangkan untuk PreparedStatement, kita perlu memberi tahu ketika membuat prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)
-    Setelah itu, untuk mendapatkan auto generate key, kita bisa menggunakan method getGeneratedKeys(), method ini akan error jika kita lupa mengirim parameter generated keys
-
-     */
-
+  
     @Test
     void testGetAutoIncrementPreparedStatement() throws SQLException {
         Connection connection = ConnectionUtil.getDataSource().getConnection();
