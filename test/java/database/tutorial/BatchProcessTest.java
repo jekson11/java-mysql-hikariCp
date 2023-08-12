1fb64aee4c175b925ae8e33a4f13b4decbc8d3c1
jekson tambunan <108537531+jekson11@users.noreply.github.com>
2023-08-12 21:27:50 +0700

    Update BatchProcessTest.java
    delete comment

--- test/java/database/tutorial/BatchProcessTest.java
+++ test/java/database/tutorial/BatchProcessTest.java
@@ -5,23 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.sql.*;
 
 public class BatchProcessTest {
-    /*
-    Batch process adalah proses mengirim perintah secara banyak sekaligus.
-    Biasanya batch process dilakukan dalam kasus tertentu saja, misal ketika kita ingin mengirim import data dari file excel ke database yang jumlahnya jutaan.
-    Biasanya dalam batch process, yang diutamakan adalah kecepatan, karena jika perintah SQL nya di execute satu satu dan menunggu response satu satu, maka sudah pasti akan sangat lambat sekali
-
-    JDBC mendukung proses eksekusi perintah SQL secara batch di Statement ataupun di PreparedStatement
-    Di Statement, terdapat method addBatch(sql) untuk menambahkan perintah ke proses batch
-    Sedangkan di PreparedStatement terdapat method addBatch() untuk menambahkan proses ke batch, lalu bisa gunakan method clearParameters() untuk menghapus parameter input user sebelumnya.
-    Setelah proses batch selesai, untuk mengeksekusinya, kita bisa gunakan perintah executeBatch()
-
-    PERINGAA!!!
-    Proses batch akan disimpan di memory sebelum dikirim ke database
-    Oleh karena itu, bijaklah membuat proses batch, jangan terlalu banyak menambahkan ke batch, misal per 100 atau per 1000
-    Jika sudah mencapai 100 atau 1000, kita bisa mengirim batch tersebut menggunakan perintah executeBatch()
-
-     */
-
+   
     @Test
     void testBatchProcessStatement() throws SQLException {
 
